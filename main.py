87ccaa1f618c76e3654434020a294f52fb5252ea
Marco <10561636@polimi.it>
2022-08-21 17:27:27 +0200

    BA_2D implementation

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import numpy as np
+import pandas as pd
+
+from maxrank import ba_hd
+from geom import *
+
+print("\n"*25)
+
 if __name__ == "__main__":
-    print("Hello World!")
+    df = pd.read_csv("examples/Test3D50/data_42.csv", index_col=0)
+    print("Loaded {} records from examples/Test3D50/data_42.csv\n".format(df.shape[0]))
+
+    data = []
+    for i in range(df.shape[0]):
+        record = df.iloc[i]
+
+        data.append(Point(record.name, record.to_numpy()))
+
+    queries = np.empty(shape=(len(data), 2), dtype=int)
+    for i in range(len(data)):
+        print("\n#  Processing data point {}  #".format(data[i].id))
+        maxrank, mincells = ba_hd(data, data[i])
+        queries[i] = [data[i].id, maxrank]
+
+    res = pd.DataFrame(queries, columns=['id', 'maxrank'])
+    res.set_index('id', inplace=True)
+    res.to_csv("examples/Test3D50/maxrank.csv")
+    print(res)
